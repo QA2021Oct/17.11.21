4b7c722d5e6a81a4d92466a50dae904ef98f2e70
QA2021Oct <92875352+QA2021Oct@users.noreply.github.com>
2021-11-17 19:26:11 +0200

    Add files via upload

---
+++ Function.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Function {
+
+    public static void main(String[] args) {
+        // functions for ordering Salad in restaurant
+        //Call method call_Waiter()
+        call_Waiter();
+        Order$();
+        make00();
+        picke();
+        serve();
+    }
+    // Create call_Waiter function
+    static void call_Waiter(){
+        String waiter = "Gabi";
+        System.out.println("Einat called the waiter");
+        if (waiter == "Gabi"){
+            System.out.println("Gabi is Ok");
+        }
+    }
+
+    static  void Order$(){
+        System.out.println("Ordered Salad");
+    }
+
+    static void make00(){
+        System.out.println("Making the Salad");
+    }
+
+    static void picke(){
+        System.out.println("waiter picked the Salad");
+    }
+
+    static  void serve(){
+        System.out.println("serve the salad");
+    }
+
+}
+
